fd3bee86fc5051c8ec081b747e97ecc38fd27210
bnros <b.ros92@gmail.com>
2023-01-04 06:18:15 -0800

    revised Fibonacci sequence and pseudocode

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,5 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
-const { listenerCount } = require("process")
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -20,7 +18,10 @@ const { listenerCount } = require("process")
 // ouput: an array with the length that it was given following the Fibonacci sequence
 
 // process:
-// create a 
+// create a function called "fibSeqNums"
+// after creating function, in the parameter, give it the value of "num"
+// in the function, declare a variable "result" and assign it to [1,1]
+// create a loop that will cycle through the fibonacci sequence that will start iterating from i=2
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -43,13 +44,14 @@ const fibLength2 = 10
 // GREEN:
 
 const fibSeqNums = (num) => {
-  let i;
-  let fib = [1, 1];
   
-  for (i = 2; i <= 10; i++) {
-    fib[i] = fib[i - 2] + fib[i - 1];
+  let result = [1, 1]
+  for (let i = 2; i < num; i++){
+    result.push(result[i-1] +result[i-2])
   }
-  return fib
+
+  return result
+  
 }
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
@@ -57,22 +59,27 @@ const fibSeqNums = (num) => {
 
 // PSEUDOCODE:
 
-// input:
-// ouput:
+// input: an object with it's values
+// ouput: an array with the study hours sorted in ascending order
 
 // process:
+// create a function called "objectValuesSort"
+// after creating function, in the parameter put in "objectValuesSort"
+// create a return line using object.values() method so that we can access the properties of the object
+// use .sort() method to have array listed from asecnding order
+
+// reflection: this first problem really stumped me, but this article helped a lot:
+// https://medium.com/quick-code/fibonacci-sequence-javascript-interview-question-iterative-and-recursive-solutions-6a0346d24053
 
 // a) Create a test with expect statements for each of the variables provided.
 
 // RED:
-
-// describe("objectValuesSorted", () => {
-//   it("sorts value of objects into an array", () => {
-//     expect(objectsValuesSorted(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
-//     expect(objectValuesSorted(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
-//   })
-// })
-
+describe("objectValuesSort", () => {
+  it("sorts value of objects into an array from least to greatest", () => {
+    expect(objectValuesSort(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(objectValuesSort(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+})
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -98,29 +105,35 @@ const studyMinutesWeek2 = {
 
 // b) Create the function that makes the test pass.
 
-// const objectsValuesSorted = () => {
+// GREEN:
+const objectValuesSort = (objectValuesSort) => {
+  return Object.values(objectValuesSort).sort((a, b) => a -b)
 
-// }
+}
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // PSEUDOCODE:
 
-// input:
-// ouput:
+// input: an array of positive and negative integars
+// ouput: a new array of numbers, with the accumulating sum
 
 // process:
+// create a function called "totalTransctions"
+// after creating function, give it the parameter of (array)
+// within function, declare a variable called "accumulatedAmount" for the array, and another variable called "transactions" to log amounts. 
+// create a loop to run througg the array and to add the values of the array
 
 // a) Create a test with expect statements for each of the variables provided.
 
 // RED:
-// describe("totalTransactions", () => {
-//   it("creates an array with the accumulating sum", () => {
-//     expect(totalTransactions(accountTransactions1)).toEqual([100, 83, 60, 51])
-//     expect(totalTransactions(accountTransactions2)).toEqual([250, 161, 261, 165])
-//     expect(totalTransactions(accountTransactions3)).toEqual([])
-//   })
-// })
+describe("totalTransactions", () => {
+  it("creates an array with the accumulating sum", () => {
+    expect(totalTransactions(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(totalTransactions(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(totalTransactions(accountTransactions3)).toEqual([])
+  })
+})
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -134,3 +147,13 @@ const accountTransactions3 = []
 // b) Create the function that makes the test pass.
 
 // GREEN:
+const totalTransactions = (array) => {
+  let accumulatedAmount = []
+  let transactions = 0
+
+  for (const num of array) {
+    transactions += num;
+    accumulatedAmount.push(transactions)
+  }
+  return accumulatedAmount
+}
